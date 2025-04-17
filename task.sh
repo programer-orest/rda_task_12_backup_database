2fea2fe264e825bb3ac1d1ad73f2bbd49217c6be
Orest Yakymiv <akimivorest@gmail.com>
2025-04-17 20:15:53 +0300

    fixed error discovered by AI

--- task.sh
+++ task.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
-DB_USER_NAME=$DB_USER
-DB_USER_PASSWORD=$DB_PASSWORD
-mysqldump -u $DB_USER_NAME -p$DB_USER_PASSWORD --databases ShopDB --result-file=backup.sql
-mysqldump -u $DB_USER_NAME -p$DB_USER_PASSWORD ShopDB --no-create-db --result-file=backup-no-create-db.sql
-mysql -u $DB_USER_NAME -p$DB_USER_PASSWORD  ShopDBReserve < backup-no-create-db.sql
-mysql -u $DB_USER_NAME -p$DB_USER_PASSWORD  ShopDBDevelopment < backup-no-create-db.sql
-
+if [ -n "$DB_USER" ] && [ -n "$DB_PASSWORD" ]; then
+  DB_USER_NAME=$DB_USER
+  DB_USER_PASSWORD=$DB_PASSWORD
+  mysqldump -u $DB_USER_NAME -p$DB_USER_PASSWORD --databases ShopDB --result-file=backup.sql
+  mysqldump -u $DB_USER_NAME -p$DB_USER_PASSWORD ShopDB --no-create-db --result-file=backup-no-create-db.sql
+  mysql -u $DB_USER_NAME -p$DB_USER_PASSWORD  ShopDBReserve < backup-no-create-db.sql
+  mysql -u $DB_USER_NAME -p$DB_USER_PASSWORD  ShopDBDevelopment < backup-no-create-db.sql
+else
+  exit 1
+fi
