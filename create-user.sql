6c12f6fa7bdb4b7bb11aa53b7c37d7c689843043
Orest Yakymiv <akimivorest@gmail.com>
2025-04-17 20:05:24 +0300

    Create script for automatically backup

--- create-user.sql
+++ create-user.sql
@@ -1,7 +1,7 @@
 CREATE USER 'backup'@'%' IDENTIFIED BY 'P@ssw0rd';
 
 -- configure minimum requored permissions for performing a db backup using mysqldump: 
-GRANT SELECT, LOCK TABLES, SHOW VIEW, PROCESS ON *.* TO 'backup'@'%';
+GRANT CREATE, DROP, SELECT, LOCK TABLES, SHOW VIEW, PROCESS ON *.* TO 'backup'@'%';
 
 -- configure required permissions to restore a database 
 GRANT ALL ON ShopDBReserve.* TO 'backup'@'%';
--- task.sh
+++ task.sh
@@ -1,1 +1,8 @@
 #! /bin/bash
+DB_USER_NAME=$DB_USER
+DB_USER_PASSWORD=$DB_PASSWORD
+mysqldump -u $DB_USER_NAME -p$DB_USER_PASSWORD --databases ShopDB --result-file=backup.sql
+mysqldump -u $DB_USER_NAME -p$DB_USER_PASSWORD ShopDB --no-create-db --result-file=backup-no-create-db.sql
+mysql -u $DB_USER_NAME -p$DB_USER_PASSWORD  ShopDBReserve < backup-no-create-db.sql
+mysql -u $DB_USER_NAME -p$DB_USER_PASSWORD  ShopDBDevelopment < backup-no-create-db.sql
+
