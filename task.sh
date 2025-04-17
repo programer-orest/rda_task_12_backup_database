89ffd3f136fce9a2ee2f0a1977f2024b3d0ae562
Orest Yakymiv <akimivorest@gmail.com>
2025-04-17 21:16:23 +0300

    add --no-create-info to clean drop error

--- task.sh
+++ task.sh
@@ -3,7 +3,7 @@ if [ -n "$DB_USER" ] && [ -n "$DB_PASSWORD" ]; then
   DB_USER_NAME=$DB_USER
   DB_USER_PASSWORD=$DB_PASSWORD
   mysqldump -u $DB_USER_NAME -p$DB_USER_PASSWORD --databases ShopDB --result-file=backup.sql
-  mysqldump -u $DB_USER_NAME -p$DB_USER_PASSWORD ShopDB --no-create-db --result-file=backup-no-create-db.sql
+  mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --no-create-db --no-create-info --result-file=backup-no-create-db-no-create-info.sql
   mysql -u $DB_USER_NAME -p$DB_USER_PASSWORD  ShopDBReserve < backup-no-create-db.sql
   mysql -u $DB_USER_NAME -p$DB_USER_PASSWORD  ShopDBDevelopment < backup-no-create-db.sql
 else
