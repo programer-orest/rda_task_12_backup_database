9d1c0e3917b0bb7e58440a4518b23d11f2733b02
Orest Yakymiv <akimivorest@gmail.com>
2025-04-18 13:57:17 +0300

    Change the code for autotest

--- task.sh
+++ task.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
-if [ -n "$DB_USER" ] && [ -n "$DB_PASSWORD" ]; then
-  DB_USER_NAME=$DB_USER
-  DB_USER_PASSWORD=$DB_PASSWORD
-  mysqldump -u $DB_USER_NAME -p$DB_USER_PASSWORD --databases ShopDB --result-file=backup.sql
-  mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --no-create-db --no-create-info --result-file=backup-no-create-db-no-create-info.sql
-  mysql -u $DB_USER_NAME -p$DB_USER_PASSWORD  ShopDBReserve < backup-no-create-db-no-create-info.sql
-  mysql -u $DB_USER_NAME -p$DB_USER_PASSWORD  ShopDBDevelopment < backup-no-create-db-no-create-info.sql
-else
-  echo 'DB_USER and DB_PASSWORD must be set'
-fi
+echo "Creating full backup of ShopDB..."
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB > ShopDB_full_backup.sql
+echo "Restoring full backup of ShopDB..."
+mysql -u "$DB_USER" -p"$DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS ShopDBReserve;"
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < ShopDB_full_backup.sql
+# Backup only data from ShopDB and restore to ShopDBDevelopment
+echo "Creating data-only backup of ShopDB..."
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-info ShopDB > ShopDB_data_backup.sql
+echo "Restoring data-only backup to ShopDBDevelopment..."
+mysql -u "$DB_USER" -p"$DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS ShopDBDevelopment;"
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < ShopDB_data_backup.sql
+
